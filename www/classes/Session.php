eb6586404cbbb67a12116f7151910c945d33e44c
Tyler11223344 <ttrebuchon@live.com>
2017-11-28 11:57:03 -0500

    Add Login() code for Session

--- www/classes/Session.php
+++ www/classes/Session.php
@@ -1,14 +1,27 @@
 <?php
 
 require_once('classes/SiteUser.php');
+require_once('classes/SQL.php');
+
+class LoginException extends Exception
+{
+    public $inner;
+
+    public function __construct($ex)
+    {
+        Parent::__construct($ex);
+        $inner = $ex;
+    }
+}
 
 class Session
 {
     public $username;
+    public $sql;
 
-
-    public function __construct()
+    public function __construct($sql)
     {
+        $this->sql = $sql;
         $this->siteUser = null;
         if ($this->isAuthenticated())
         {
@@ -18,7 +31,35 @@ class Session
 
     public function isAuthenticated()
     {
-        return (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true);
+        return (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true);
+    }
+
+    public function Login($username, $password)
+    {
+        try
+        {
+            if (!$this->sql->siteUserExists($username))
+            {
+                throw new Exception('User does not exist!');
+            }
+
+            $hash = $this->sql->getPasswordHash_ByName($username);
+            
+            if (SiteUser::verifyPassword($hash, $password))
+            {
+                $this->username = $username;
+                $_SESSION['username'] = $this->username;
+                $_SESSION['loggedin'] = true;
+            }
+            else
+            {
+                throw new Exception('Invalid password!');
+            }
+        }
+        catch (Exception $ex)
+        {
+            throw new LoginException($ex);
+        }
     }
 }
 
