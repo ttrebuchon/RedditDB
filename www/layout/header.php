62560c2469b4475fb22e2926f6abf7d8fb026c2f
Tyler11223344 <ttrebuchon@live.com>
2017-11-28 13:05:18 -0500

    Make style path absolute

--- www/layout/header.php
+++ www/layout/header.php
@@ -5,7 +5,7 @@
 <head>
     <title><?php if (isset($title)) { echo $title; }?></title>
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="/style/main.css">
 </head>
 
 <body>
