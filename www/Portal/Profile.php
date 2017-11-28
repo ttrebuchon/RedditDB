7de52f4d24fe334dfdfa9ac3dcc5455aef983198
Tyler11223344 <ttrebuchon@live.com>
2017-11-28 14:30:23 -0500

    Add header and footer to Profile.php

--- www/Portal/Profile.php
+++ www/Portal/Profile.php
@@ -1,4 +1,15 @@
 <?php
+require_once(__DIR__ . '/../' . 'includes/config.php');
 
+
+$title = 'Profile';
+require(__DIR__ . '/../' . 'layout/header_auth.php');
+
+?>
+
+
+
+<?php
+require(__DIR__ . '/../' . 'layout/footer.php');
 
 ?>
