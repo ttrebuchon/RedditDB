19d264bf5753a6069a8b29d6b75786c8a3b6da42
Tyler11223344 <ttrebuchon@live.com>
2017-11-29 15:12:30 -0500

    Home Page work

--- www/Portal/Home.php
+++ www/Portal/Home.php
@@ -7,6 +7,57 @@ require(__DIR__ . '/../' . 'layout/header_auth.php');
 
 ?>
 
+<table class="ExpandTable">
+    <tr>
+        <td>
+            <label>Watched Users</label>
+            <div class="DataBox">
+                <table id="WatchedUsers" border=1 class="ExpandTable">
+                    <tr>
+                        <td>Name</td>
+                    </tr>
+                </table>
+            </div>
+        </td>
+        <td>
+            <label>Watched Subreddits</label>
+            <div class="DataBox">
+                <table id="WatchedSubreddits" border=1 class="ExpandTable">
+                    <tr>
+                        <td>Name</td>
+                    </tr>
+                </table>
+            </div>
+        </td>
+        <td>
+            <label>Watched Posts</label>
+            <div class="DataBox">
+                <table id="WatchedPosts" border=1 class="ExpandTable">
+                    <tr>
+                        <td>ID</td>
+                        <td>Name</td>
+                    </tr>
+                </table>
+            </div>
+        </td>
+        <td>
+            <label>Watched Comments</label>
+            <div class="DataBox">
+                <table id="WatchedComments" border=1 class="ExpandTable">
+                    <tr>
+                        <td>ID</td>
+                        <td>Text</td>
+                    </tr>
+                </table>
+            </div>
+        </td>
+    </tr>
+</table>
+
+
+
+
+
 <?php
 require(__DIR__ . '/../' . 'layout/footer.php');
 
